0d60444111d95aaf5531b4f709ad268ee32d4665
Giovanny Rueda <giovannirueda588@gmail.com>
2025-11-30 17:56:35 -0500

    Add files via upload

---
+++ ejercicio 7.py
@@ -0,0 +1,29 @@
+﻿#*********convertir celsius a fahrenheit*********
+#*********zona de funciones************
+
+def definir_celsius():
+    celsius = float(input("Digite el valor en grados Celsius: "))
+    return celsius
+
+def convertir_a_fahrenheit(celsius):
+    fahrenheit = float((celsius * 9/5) + 32)
+    return fahrenheit
+
+def hacer_mensaje(celsius, fahrenheit):
+    mensaje = str(celsius) + " Celsius = " + str(fahrenheit) + " Fahrenheit"
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+
+#*******codigo principal********
+def main():
+    celsius = definir_celsius()
+    fahrenheit = convertir_a_fahrenheit(celsius)
+    mensaje = hacer_mensaje(celsius, fahrenheit)
+    imprimir_mensaje(mensaje)
+
+
+if __name__ == '__main__':
+    main()
