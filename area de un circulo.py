e6cf84cc2044af056dafd0a572f378bfc107a32c
Giovanny Rueda <giovannirueda588@gmail.com>
2025-11-30 17:36:21 -0500

    ejercicio 5

---
+++ area de un circulo.py
@@ -0,0 +1,28 @@
+#**********area de un circulo*********
+#**********zona de funciones**********
+
+def definir_radio():
+    radio = float(input("Digite el valor del radio: "))
+    return radio
+    
+def definir_area (radio):
+    area = 3.1416 * radio ** 2
+    return area
+    
+def hacer_mensaje (area):
+    mensaje = "El área del círculo es: " + str(area)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+    
+#*******codigo principal********
+def main():
+    radio = definir_radio()
+    area = definir_area(radio)
+    mensaje = hacer_mensaje(area)
+    imprimir_mensaje(mensaje)
+
+
+if __name__ == '__main__':
+    main()
