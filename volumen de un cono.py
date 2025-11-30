a600212126f81ac6a99764c5d497c017dd0e9dc5
Giovanny Rueda <giovannirueda588@gmail.com>
2025-11-30 17:55:44 -0500

    Add files via upload
    ejercicio 6

---
+++ volumen de un cono.py
@@ -0,0 +1,34 @@
+﻿#*********volumen de un cono*********
+#*********zona de funciones************
+
+def definir_radio():
+    radio = float(input("Digite el valor del radio: "))
+    return radio
+
+def definir_altura():
+    altura = float(input("Digite el valor de la altura: "))
+    return altura
+
+def definir_volumen(radio, altura):
+    volumen = float((1/3) * 3.1416 * radio**2 * altura)
+    return volumen
+
+def hacer_mensaje(volumen):
+    mensaje = "El volumen del cono es: " + str(volumen)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+
+
+#*******codigo principal********
+def main():
+    radio = definir_radio()
+    altura = definir_altura()
+    volumen = definir_volumen(radio, altura)
+    mensaje = hacer_mensaje(volumen)
+    imprimir_mensaje(mensaje)
+
+
+if __name__ == '__main__':
+    main()
