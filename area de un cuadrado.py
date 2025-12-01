64af2b9c8f51f4527fae026a83bb9e9c04238b03
Giovanny Rueda <giovannirueda588@gmail.com>
2025-12-01 07:18:38 -0500

    Add files via upload

---
+++ area de un cuadrado.py
@@ -0,0 +1,27 @@
+#******area cuadrado ******
+#*******zona de funciones ********
+def capturar_lado ():
+    lado = int(input("digite el valor del lado: "))
+    return lado
+
+def hacer_area(lado):
+    area= (lado * lado)
+    return area
+
+def hacer_mesaje(area ):
+    mensaje = "la area del cuadrado es: " + str(area)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+    
+#****zona codigo principal*****
+def main() :
+    lado = capturar_lado()
+    area = hacer_area(lado)
+    mensaje = hacer_mesaje(area)
+    imprimir_mensaje(mensaje)
+
+if __name__ == '__main__':
+    main()
+    
