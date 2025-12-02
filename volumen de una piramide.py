45331db7573d95b5026714522c55fee05d0055ba
Giovanny Rueda <giovannirueda588@gmail.com>
2025-12-02 06:31:54 -0500

    Add files via upload

---
+++ volumen de una piramide.py
@@ -0,0 +1,38 @@
+#*****volumen piramide*****
+#*****zona funcional*****
+
+def capturar_longitud():
+    longitud = int(input("digite el valor de la longitud de la base: ")) 
+    return longitud
+
+def capturar_ancho():
+    ancho = int(input("digite el valor del ancho de la base: ")) 
+    return ancho
+
+def capturar_altura():
+    altura = int(input("digite el valor de la altura: ")) 
+    return altura
+
+def hacer_volumen(longitud, ancho, altura):
+    volumen = ( 1/3 * longitud * altura * ancho)
+    return volumen
+
+def hacer_mesaje(volumen):
+    mensaje = "el volumen de una piramide es: " + str(volumen)
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+    
+#****zona codigo principal*****
+
+def main():
+    longitud = capturar_longitud()
+    ancho = capturar_ancho()
+    altura = capturar_altura()
+    volumen = hacer_volumen(longitud, ancho, altura)
+    mensaje = hacer_mesaje(volumen)
+    imprimir_mensaje(mensaje)
+
+if __name__ == '__main__':
+    main()
