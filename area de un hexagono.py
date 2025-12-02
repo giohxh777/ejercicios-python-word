2a87e8e2f6c97edf3f1d54d801d2f83f6a46f34f
Giovanny Rueda <giovannirueda588@gmail.com>
2025-12-02 06:28:09 -0500

    Add files via upload

---
+++ area de un hexagono.py
@@ -0,0 +1,29 @@
+#******area de un paralelogramo ******
+#*******zona de funciones ********
+def capturar_base ():
+    perimetro = int(input("digite el valor de la perimetro: "))
+    return perimetro
+
+def capturar_apotema ():
+    apotema = int(input("digite el valor de la apotema: "))
+    return apotema
+
+def hacer_area (perimetro, apotema):
+    area = (perimetro * apotema ) / 2
+    return area
+
+def hacer_mensaje(area):
+    mensaje = "la area del hexagono es: " + str(area)
+    return mensaje 
+
+def imprimir_mensaje(mensaje):
+    print (mensaje)
+    
+    
+#***** zona codigo principal*******
+
+perimetro = capturar_base()
+apotema = capturar_apotema()
+area = hacer_area(perimetro, apotema)
+mensaje = hacer_mensaje(area)
+imprimir = imprimir_mensaje(mensaje)
